8621e32e3f32d04bf13bf2c20e34ad30e044026a
miladBentaiba <bm_bentaiba@esi.dz>
2019-06-28 13:24:26 +0100

    put calendar in a card

--- frontend/src/views/Calendar.jsx
+++ frontend/src/views/Calendar.jsx
@@ -6,18 +6,29 @@ import FullCalendar from '@fullcalendar/react'
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
 
+import {Card, CardHeader, Row, Col} from "reactstrap";
+
 export default class CalendarFull extends React.Component {
 
   render() {
     return (
-      <div className="content">  
-        <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} 
-        events={[
-          { title: 'event 1', date: '2019-06-01' },
-          { title: 'event 2', date: '2019-06-29' },
-          { title: 'jasser 2', date: '2019-06-29' }]}/>
+      <>
+      <div className="content">
+       <Row>
+          <Col md="12">
+              <Card>
+             <CardHeader>
+             <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} 
+                events={[
+                  { title: 'event 1', date: '2019-06-01' },
+                  { title: 'event 2', date: '2019-06-29' },
+                  { title: 'jasser 2', date: '2019-06-29' }]}/>
+              </CardHeader>
+            </Card>
+          </Col> 
+        </Row>
       </div>
-      
+    </>
     )
   }
 
